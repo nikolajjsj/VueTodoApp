0a89a84658b2c800c52f8b5bd9170bce2f71da9e
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2020-11-19 15:34:15 +0100

    Basic functionality for a todo app done - with the composition API in Vue v3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue Todo App</h1>
+  <form @submit.prevent="addNewTodo">
+    <label for="newTodo">New Todo</label>
+    <input v-model="newTodo" name="newTodo" />
+    <button>Add New Todo</button>
+  </form>
+  <button v-if="todos.length > 0" @click="markAllDone">Mark All Done</button>
+  <button v-if="todos.length > 0" @click="removeAllTodos">Remove All</button>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+      <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
+        {{ todo.content + " - " }}
+        <strong @click="removeTodo(index)">X</strong>
+      </h3>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  setup() {
+    const newTodo = ref('');
+    const todos = ref([]);
+
+    function addNewTodo() {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = '';
+    }
+
+    function toggleDone(todo) {
+      // eslint-disable-next-line no-param-reassign
+      todo.done = !todo.done;
+    }
+
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    }
+
+    function markAllDone() {
+      todos.value.forEach((todo) => {
+        // eslint-disable-next-line no-param-reassign
+        todo.done = true;
+      });
+    }
+
+    function removeAllTodos() {
+      todos.value = [];
+    }
+
+    return {
+      todos,
+      newTodo,
+      addNewTodo,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAllTodos,
+    };
   },
 };
 </script>
@@ -21,6 +74,34 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-size: 2em;
+  width: 80%;
+  margin: 0 auto;
+}
+
+input,
+textarea,
+button,
+p,
+div,
+section,
+section,
+article,
+select {
+  display: block;
+  width: 100%;
+  font-size: 1em;
+  margin: 0.5em;
+  box-sizing:border-box
+}
+
+.done {
+  text-decoration: line-through;
+}
+
+.todo {
+  cursor: pointer;
 }
 </style>
